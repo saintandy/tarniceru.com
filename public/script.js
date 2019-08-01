0d6b467309a404aead2db8d9620873e90e7c12a6
saintandy <vladtarniceru@gmail.com>
2019-08-01 19:55:58 +0000

    1.0

---
+++ public/script.js
@@ -0,0 +1,50 @@
+let windowWidth = window.innerWidth;
+let styleElement = document.createElement('style');
+
+function appendFilter() {
+  styleElement.innerHTML = `
+  #filter {
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(
+      -135deg,
+      rgba(255, 255, 255, 0.28) 25%,
+      rgba(255, 255, 255, 0.7) calc(25% + 1px),
+      rgba(255, 255, 255, 0.225) calc(25% + 1px),
+      rgba(255, 255, 255, 0.225) calc(50%),
+      rgba(255, 255, 255, 0.7) calc(50% + 1px),
+      rgba(255, 255, 255, 0.15) calc(50% + 1px),
+      rgba(255, 255, 255, 0.15) calc(75%),
+      rgba(255, 255, 255, 0.7) calc(75% + 1px),
+      rgba(255, 255, 255, 0.1) calc(75% + 1px),
+      rgba(255, 255, 255, 0.1) calc(100%)
+    )
+  }
+  `;
+  document.head.appendChild(styleElement);
+}
+
+function removeFilter() {
+  document.head.removeChild(styleElement);
+}
+
+function adaptWidthToFontSize() {
+  if (windowWidth < 400) {
+    document.body.style.fontSize = "8px";
+  } else if (windowWidth < 500) {
+    document.body.style.fontSize = "10px";
+  } else if (windowWidth < 700) {
+    document.body.style.fontSize = "12px";
+  } else {
+    document.body.style.fontSize = "16px";
+  }
+}
+
+appendFilter();
+adaptWidthToFontSize();
+window.addEventListener('resize', () => {
+  windowWidth = window.innerWidth;
+  adaptWidthToFontSize();
+});
+
+
